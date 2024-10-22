8ccb0219b7aa08b9bfe89072e601e9171ed60b93
aldoly <alvaradogasa@gmail.com>
2024-10-21 22:50:34 -0600

    Tarea 2

---
+++ EjPractico2/src/main/java/com/ejpractico2/EjPractico2/EjPractico2Application.java
@@ -0,0 +1,13 @@
+package com.ejpractico2.EjPractico2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjPractico2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjPractico2Application.class, args);
+	}
+
+}
---
+++ EjPractico2/src/test/java/com/ejpractico2/EjPractico2/EjPractico2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejpractico2.EjPractico2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjPractico2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
